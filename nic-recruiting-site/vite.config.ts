472abc225c8060458bab71c1b705260e809a01b4
nicbramble <56664866+nicbramble@users.noreply.github.com>
2025-08-31 20:25:02 -0600

    Delete nic-recruiting-site directory

--- nic-recruiting-site/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Default base '/' for local dev. The GitHub Action passes --base="/<repo>/" automatically.
-export default defineConfig({
-  plugins: [react()],
-  base: '/'
-})
